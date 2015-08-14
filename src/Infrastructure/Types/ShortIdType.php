b4de9c2bc81a5cb0227a44c370c3415c3d284fbb
Danijel Maricic <danijel.maricic@nordsoftware.com>
2015-08-14 17:36:37 +0300

    cleaned up custom Type

--- src/Infrastructure/Types/ShortIdType.php
+++ src/Infrastructure/Types/ShortIdType.php
@@ -1,6 +1,7 @@
 <?php
 namespace Nord\Lumen\Doctrine\ODM\MongoDB\Infrastructure\Types;
 
+use Crisu83\ShortId\ShortId;
 use Doctrine\ODM\MongoDB\Types\Type;
 use Nord\Lumen\Doctrine\ODM\MongoDB\Domain\Model\ShortId as ShortIdModel;
 
@@ -14,11 +15,7 @@ final class ShortIdType extends Type
      */
     public function convertToPHPValue($value)
     {
-        // Note: this function is only called when your custom type is used
-        // as an identifier. For other cases, closureToPHP() will be called.
-        //return new \DateTime('@' . $value->sec);
-        $phpValue = new ShortIdModel($value);
-        return $phpValue;
+        return $value instanceof ShortId ? (string) $value : $value;
     }
 
     /**
@@ -26,9 +23,7 @@ final class ShortIdType extends Type
      */
     public function closureToPHP()
     {
-        // Return the string body of a PHP closure that will receive $value
-        // and store the result of a conversion in a $return variable
-        return '$return = new ShortIdModel($value)';
+        return '$return = new ShortIdModel($value);';
     }
 
 
@@ -37,9 +32,13 @@ final class ShortIdType extends Type
      */
     public function convertToDatabaseValue($value)
     {
-        // This is called to convert a PHP value to its Mongo equivalent
-        //return new \MongoDate($value);
         return $value instanceof ShortIdModel ? $value->getValue() : $value;
 
+    }
+
+
+    public function closureToMongo()
+    {
+        return '$return = new ShortIdModel($value);';
     }
 }
